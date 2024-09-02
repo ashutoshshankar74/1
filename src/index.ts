83cfad92e79e54befe55d6503005b7bd4226cec4
ashutoshshankar74 <ashutoshshankar74@gmail.com>
2024-09-02 18:35:43 +0530

    updated code

--- src/index.ts
+++ src/index.ts
@@ -3,23 +3,27 @@ import cors from 'cors';
 import userRoutes from './routes/user';
 import notificationRoutes from './routes/notification';
 import 'dotenv/config';
-
 import notificationWorker from './workers/notificationWorker';
 
 const app = express();
-const port = process.env.PORT || 3000;
+
+// Convert process.env.PORT to a number
+const port = parseInt(process.env.PORT || '3000', 10);
 
 app.use(cors());
 app.use(express.json());
 
 app.use('/api', userRoutes);
 app.use('/api', notificationRoutes);
+
 console.log('Index.js is executed properly');
 
-
 // Log the actual address where the server is running
-app.listen(port, () => {
-  const host = process.env.RENDER_EXTERNAL_HOST ;
+app.listen(port, '0.0.0.0', () => {
+  const host = process.env.RENDER_EXTERNAL_HOST || 'localhost';
   console.log(`Server is running on http://${host}:${port}`);
 });
+
+// Start the notification worker
 notificationWorker;
+console.log("index.js comes to an end");
